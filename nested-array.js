ef3e05d52129d0a8cdc740b4c28c17188030b734
D. Rhesa Prawedha <dr.prawedha@gmail.com>
2018-11-09 21:16:29 +0700

    Latihan Nested Array

---
+++ nested-array.js
@@ -0,0 +1,13 @@
+function panggilNestedArray() {
+  var datanama = [["Ab","Ac","Ad"],
+                  ["Bola1","Bola2","Bola3"],
+                  ["Rel1","Res2","Ret3"]]
+  console.log("Banyaknya data nama "+datanama.length);
+  console.log("Data nama pertama adalah "+datanama[0]);
+  console.log("Data nama pertama poin pertama adalah "+datanama[0][0]);
+  console.log("Data nama kedua adalah "+datanama[1]);
+  console.log("Data nama ketiga poin kedua adalah "+datanama[2][1]);
+
+}
+
+panggilNestedArray()
