4f1814f4aea4033d142ad43dc937dd1985d5c8f9
D. Rhesa Prawedha <dr.prawedha@gmail.com>
2018-11-09 20:48:56 +0700

    Latihan switch

---
+++ switch.js
@@ -0,0 +1,18 @@
+function testingSwitch() {
+  var nilai = 8;
+  //hampir sama dengan if else
+  switch (nilai) {
+    case 10:
+      console.log("Sangat Bagus");
+      break;
+    case 8:
+      console.log("Baik");
+      break;
+    case 6:
+      console.log("Cukup");
+      break;
+    default:
+      console.log("Lainnya");
+  }
+}
+testingSwitch()
