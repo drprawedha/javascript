51cd9abb123e8a1470862cc5ea4161dea9f11f5c
D. Rhesa Prawedha <dr.prawedha@gmail.com>
2018-11-09 21:36:01 +0700

    Latihan Operator

---
+++ operators.js
@@ -0,0 +1,10 @@
+function panggilOperators() {
+  var a = 4;
+  var b = 3;
+
+  //console.log('penjumlahan : ' + a +' + '+ b +' = '+ (a+b));
+  return a*b;
+}
+
+//panggilOperators()
+console.log(panggilOperators());
