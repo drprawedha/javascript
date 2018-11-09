1f0e91d9bc7d9d4b3dc65000876f5103fe7b22c2
D. Rhesa Prawedha <dr.prawedha@gmail.com>
2018-11-09 20:14:36 +0700

    Latihan Function

---
+++ function.js
@@ -0,0 +1,20 @@
+function jalansatu() {
+  console.log("jalan satu");
+}
+jalansatu()
+
+
+const jalandua = function () {
+  console.log("jalan dua");
+}
+jalandua()
+
+// dengan menggunakan konstanta agar function tak daat dirubah
+// walaupun bisa dengan var , atau let.
+// dua diatas merupakan mode penulisan lama
+// sedangkan dibawah menggunakan mode penulisan baru
+
+const jalantiga = () => {
+  console.log("jalan tiga");
+}
+jalantiga()
