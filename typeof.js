fce0a3be8e5e83363a7635bfedce682b86a6a182
D. Rhesa Prawedha <dr.prawedha@gmail.com>
2018-11-09 21:21:56 +0700

    Latihan Type data

---
+++ typeof.js
@@ -0,0 +1,17 @@
+function checkTypeData() {
+  var data1 = "Nama";
+  var data2 = 20;
+  var data3 = [1,2,3];
+  var data4 = {
+    nama : "andi",
+    usia : 20,
+  }
+
+
+  console.log("tipe data1 nama adalah " + typeof(data1));
+  console.log("tipe data2 nama adalah " + typeof(data2));
+  console.log("tipe data3 nama adalah " + typeof(data3));
+  console.log("tipe data4 nama adalah " + typeof(data4));
+}
+
+checkTypeData()
