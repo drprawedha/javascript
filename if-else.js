61a320faf3d5d327fa6e82e721580e71eeb9503a
D. Rhesa Prawedha <dr.prawedha@gmail.com>
2018-11-09 20:26:33 +0700

    Latihan If Else

---
+++ if-else.js
@@ -0,0 +1,21 @@
+function jalansatu() {
+  var nilai = 3;
+  if (nilai === 3) {
+    //jika nilai = 2 maka if else tak berlaku
+    //jika nilai == 2 maka if else sudah berlaku
+    //jika nilai === 2 maka if else sudah berlaku
+    console.log("Benar, Nilai tiga");
+  } else if (nilai===2) {
+    console.log("Hanya Nilai Dua");
+    } else {
+      console.log("Maaf, Salah");
+      }
+}
+jalansatu()
+
+function jalandua() {
+  var nilai = 2;
+  if(nilai===1)console.log("OK, Nilai satu")
+  else console.log("Bukan, Nilai satu");
+}
+jalandua()
