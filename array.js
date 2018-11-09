c9df6f087c95f2859b8c242fd6277e880397ed30
D. Rhesa Prawedha <dr.prawedha@gmail.com>
2018-11-09 21:15:15 +0700

    Latihan Array

---
+++ array.js
@@ -0,0 +1,13 @@
+function panggilArray() {
+  var data = [1,2,3,4,5,9]
+  var nama = ["ali","babe","cucu"]
+
+  console.log("Banyak data "+data.length);
+  console.log("Banyak nama "+nama.length);
+
+  console.log("Nama pertama adalah "+nama[0]);
+  console.log("Nama kedua adalah "+nama[1]);
+  console.log("Nama ketiga adalah "+nama[2]);
+}
+
+panggilArray()
