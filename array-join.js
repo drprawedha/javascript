0c6816f510d10ad3055ad078817624bbf32b78e3
D. Rhesa Prawedha <dr.prawedha@gmail.com>
2018-11-19 19:27:07 +0700

    Latihan Array Join

---
+++ array-join.js
@@ -0,0 +1,8 @@
+function panggilJoin() {
+  var kota = ["jakarta","bandung","semarang","surabaya"];
+  console.log(kota);
+  var result = kota.join(" ");
+  return result;
+}
+
+console.log(panggilJoin());
