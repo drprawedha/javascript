06b2bafc60e7956fed716ddc47dd1e14de7bd5e1
D. Rhesa Prawedha <dr.prawedha@gmail.com>
2018-11-19 08:37:15 +0700

    Latihan Array-Unshift

---
+++ array-unshift.js
@@ -0,0 +1,8 @@
+function panggilUnshift() {
+  var kota = ["jakarta","bandung","malang","surakarta"];
+  console.log(kota);
+  kota.unshift("surabaya","bali");
+  return kota;
+}
+
+console.log(panggilUnshift());
