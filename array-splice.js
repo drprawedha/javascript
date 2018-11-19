e03b27bead3f0530c83e24bba303e95f2a3070c6
D. Rhesa Prawedha <dr.prawedha@gmail.com>
2018-11-19 08:19:25 +0700

    Latihan Array Splice

---
+++ array-splice.js
@@ -0,0 +1,13 @@
+function panggilSplice() {
+  var kota = ['jakarta','medan','padang','malang'];
+  console.log(kota[0]);
+
+  // splice menyisipkan data kedalam array sesuka kita
+  kota.splice(2,1,'denpasar')
+  // 2 berarti no array kedua
+  // 0 berarti data tidak dihapus, namun jika diganti
+  // 1 berarti data dihapus
+  return kota;
+}
+
+console.log(panggilSplice());
